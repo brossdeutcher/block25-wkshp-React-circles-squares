b2bf5b819a9bc4910867618fb130b8e5f697cae4
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-07-25 14:17:07 -0600

    removed a console.log() & renamed the repo

--- workshop.js
+++ workshop.js
@@ -1,43 +1,63 @@
 const container = document.querySelector(`#root`);
 
 const App = () => {
-
   const generateColor = () => {
-    const chars = '0123456789abcdef';
-    var color = '#';
-    for (let i=0; i<6; i++) {
-      color += chars[Math.floor(Math.random()*16)];
+    const chars = "0123456789abcdef";
+    var color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += chars[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+  };
 
   const generateSquare = () => {
-    const dimensions = Math.ceil((Math.random() * 100) + 100);
-    return <div style={{height: dimensions, width: dimensions, backgroundColor: generateColor()}}></div>
-  }
+    const dimensions = Math.ceil(Math.random() * 100 + 100);
+    return (
+      <div
+        style={{
+          height: dimensions,
+          width: dimensions,
+          backgroundColor: generateColor(),
+        }}
+      ></div>
+    );
+  };
 
-  return (<>
-    <Circles colorGenerator={generateColor} />
-    <section id="squares">
-    {generateSquare()}
-    {generateSquare()}
-    {generateSquare()}
-    </section>
-  </>)
-}
+  return (
+    <>
+      <Circles colorGenerator={generateColor} />
+      <section id="squares">
+        {generateSquare()}
+        {generateSquare()}
+        {generateSquare()}
+      </section>
+    </>
+  );
+};
 
 const Circles = (props) => {
   const generateColor = props.colorGenerator;
   const generateCircle = () => {
-    const diameter = Math.ceil((Math.random() * 100) + 100);
-    return <div style={{height: diameter, width: diameter, backgroundColor: generateColor(), borderRadius: '50%'}}></div>
-  }
-  return (<section id="circles">
-    {generateCircle()}
-    {generateCircle()}
-    {generateCircle()}
-  </section>)
-}
+    const diameter = Math.ceil(Math.random() * 100 + 100);
+    return (
+      <div
+        style={{
+          height: diameter,
+          width: diameter,
+          backgroundColor: generateColor(),
+          borderRadius: "50%",
+        }}
+      ></div>
+    );
+  };
+  return (
+    <section id="circles">
+      {generateCircle()}
+      {generateCircle()}
+      {generateCircle()}
+    </section>
+  );
+};
 
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
