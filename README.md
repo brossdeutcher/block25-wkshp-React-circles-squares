15636a5e9dc237c3a8b5ac04566f1d8f20cb21f9
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-07-25 13:45:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Circle & Square Generator
+
+## Using React to generate three circles and three squares
++ diameters and width/height are randomly generated between 100 and 200
++ Colors are generated randomly using hex color codes
---
+++ workshop.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(`#root`);
+
+const App = () => {
+
+  const generateColor = () => {
+    const chars = '0123456789abcdef';
+    var color = '#';
+    for (let i=0; i<6; i++) {
+      color += chars[Math.floor(Math.random()*16)];
+    }
+    return color;
+  }
+
+  const generateCircle = () => {
+    const diameter = Math.ceil((Math.random() * 100) + 100);
+    return <div style={{height: diameter, width: diameter, backgroundColor: generateColor(), borderRadius: '50%'}}></div>
+  }
+
+  const generateSquare = () => {
+    const dimensions = Math.ceil((Math.random() * 100) + 100);
+    return <div style={{height: dimensions, width: dimensions, backgroundColor: generateColor()}}></div>
+  }
+
+  return (<>
+    {generateCircle()}
+    {generateCircle()}
+    {generateCircle()}
+    {generateSquare()}
+    {generateSquare()}
+    {generateSquare()}
+  </>)
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
